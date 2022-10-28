3b0e318c928f66470696fa949f85c5420a29b4e5
islam kamel <islam.kamel@agr.svu.edu.eg>
2022-10-28 04:01:51 +0200

    Create Storage Class

--- github.py
+++ github.py
@@ -1,32 +1,37 @@
 import platform
-import sys
-
 import click
 import requests
 from excaption import NotValidURL
 from access import github_access
 from update import download, get_sys_info
 import subprocess
+from storge import Storage
 
 
-class Github:
-    def __init__(self,  url):
+class Github(Storage):
+    def __init__(self, url):
+        super().__init__()
         self.__url = self.url = url
         self.ext = None
         self.__app_list = list()
         self.name = url.split('/')[-1]
+        self.__is_exists(url)
+
 
     @property
     def ext(self):
         return self.__ext
 
+
     @ext.setter
     def ext(self, value):
         self.__ext = value
 
+
     @property
     def url(self):
         return self.__url
+
 
     @url.setter
     def url(self, value):
@@ -36,6 +41,7 @@ class Github:
         else:
             raise NotValidURL(value)
 
+
     def get_latest_release(self) -> str:
         release = requests.get(self.url, headers=github_access())
         if release.status_code == 200:
@@ -43,10 +49,12 @@ class Github:
             return release['assets_url']
         raise NotValidURL()
 
+
     def get_latest_assets(self) -> None:
         assets_url = self.get_latest_release()
         assets = requests.get(assets_url, headers=github_access())
         self.__app_list = assets.json()
+
 
     def find_app(self) -> dict:
         platform_info = get_sys_info()
@@ -59,6 +67,7 @@ class Github:
                 if sys_type in app['name'] and app['name'].endswith('msi'):
                     return app
 
+
     def install(self):
         self.get_latest_assets()
         app = self.find_app()
@@ -66,3 +75,13 @@ class Github:
         download(app['browser_download_url'], app['name'])
         subprocess.check_call(f'{app["name"]}', shell=True, stdout=subprocess.DEVNULL)
         click.secho('🎉 Installed Success', fg='yellow')
+
+
+    def __is_exists(self, url):
+        doc_id = url.split('/')[0].lower()
+        if self.is_exists(doc_id):
+            return True
+        return False
+
+
+g = Github('PowerShell/powershell')
--- storge.py
+++ storge.py
@@ -1,20 +1,62 @@
 import firebase_admin
 from firebase_admin import firestore
-from firebase_admin import get_app
+import os
 
-# Application Default credentials are automatically created.
-config = {
-    'apiKey': "AIzaSyAayjmvIcb7cwojSNZH2h5h3I2yrfrx-CM",
-    'authDomain': "update-manager-c767d.firebaseapp.com",
-    'projectId': "update-manager-c767d",
-    'storageBucket': "update-manager-c767d.appspot.com",
-    'messagingSenderId': "859352236545",
-    'appId': "1:859352236545:web:4263d5fb299a747b31b600",
-    'measurementId': "G-5EZ124GW63"
-}
 
-# cred_obj = firebase_admin.credentials.Certificate('./cred.json')
-app = firebase_admin.initialize_app()
-# app = firebase_admin.get_app('package')
-db = firestore.client()
-# print(app.credential)
+class Storage:
+    """
+    Document Func
+    'collection', 'collections', 'create', 'delete', 'get', 'id', 'on_snapshot',
+    'parent', 'path', 'set', 'update'
+
+    Collection
+    Func 'add', 'document', 'end_at', 'end_before', 'get', 'id', 'limit',
+    'limit_to_last', 'list_documents', 'offset', 'on_snapshot', 'order_by',
+    'parent', 'recursive', 'select', 'start_after', 'start_at', 'stream',
+    'where'
+    """
+
+
+    def __init__(self):
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'E:\my-developer\cli_package_manager\update-manager-c767d-firebase-adminsdk-ukjho-bc56cea676.json'
+        self.__app = firebase_admin.initialize_app()
+        self.__db = firestore.client()
+
+
+    def add_package(self, package_id, **kwargs) -> firestore:
+        new_package = self.__db.collection('packages').document(package_id)
+        new_package.set(kwargs)
+        return new_package
+
+
+    def get_all(self) -> list:
+        packages = self.__db.collection('packages').stream()
+        elements = []
+        for package in packages:
+            elements.append(package)
+        return elements
+
+
+    def get_package(self, package_id) -> firestore:
+        package = self.__db.collection('packages').document(package_id)
+        return package
+
+
+    def is_exists(self, package_id):
+        package = self.__db.collection('packages').document(package_id)
+        if package.get().to_dict() is not None:
+            return True
+        return False
+
+
+    def update_package(self, package_id, **kwargs):
+        if self.is_exists(package_id):
+            package = self.get_package(package_id)
+            package.update(kwargs)
+            return package
+        raise 'Not Found Package 404.'
+
+
+    def delete_package(self, package_id):
+        package = self.get_package(package_id)
+        package.delete()
